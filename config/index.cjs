b70ba6ed054b754ca0259ef1ebcb9e30c477f48a
Mystery413 <106495125+Mystery413@users.noreply.github.com>
2024-05-05 11:25:10 +1200

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -62,7 +62,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'xxy小朋友',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'od6iG6hCljpJgH7w1oDG8XaKq__Y',
+      id: 'od6iG6pdIYuAad6WtI9BK079q-AA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'p982JTw9kNyXgv_pbhz8d5hQyiHxN-WUWJXLVEaZz0s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
