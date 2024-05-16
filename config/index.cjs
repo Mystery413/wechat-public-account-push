48c0709f4970e121b67350c9d5a33b59b9b4219a
Mystery413 <106495125+Mystery413@users.noreply.github.com>
2024-05-17 11:33:01 +1200

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -64,7 +64,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'od6iG6hCljpJgH7w1oDG8XaKq__Y',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ytFVtLaxt4G5ic8NCSPQ-8Q_cTI_7j9Ow9gSf-GGZBM',
+      useTemplateId: 'aTaX7sJF2OWPselenPv7YiHpfyCH4euivW1G9ClZXiU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-27',
       festivals: [
